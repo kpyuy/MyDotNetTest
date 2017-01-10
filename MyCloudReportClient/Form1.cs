6c7994cfb658f1dfce4fa408ffc32d5daecf9ddc
yuzy <yuzy@mysoft.com.cn>
2017-01-10 23:26:33 +0800

    test

--- MyCloudReportClient/Form1.cs
+++ MyCloudReportClient/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 
 namespace MyCloudReportClient
@@ -21,9 +22,58 @@ namespace MyCloudReportClient
         private void button1_Click(object sender, EventArgs e)
         {
             var factory = new ConnectionFactory();
+            factory.HostName = "192.168.1.7";
             factory.UserName = "admin";
             factory.Password = "123456";
+            factory.Port = 5672;
 
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+                    string message = "Hello World";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish("", "hello", null, body);
+                    Log(string.Format("Send {0}", message));
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "192.168.1.7";
+            factory.UserName = "admin";
+            factory.Password = "123456";
+            factory.Port = 5672;
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    var consumer = new QueueingBasicConsumer(channel);
+                    channel.BasicConsume("hello", true, consumer);
+                    Log("waiting for message");
+
+                    while (true)
+                    {
+                        var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Log(string.Format("Received {0}", message));
+
+                    }
+                }
+            }
+        }
+
+        private void Log(string msg)
+        {
+            textBox1.AppendText(msg + Environment.NewLine);
         }
     }
 }
